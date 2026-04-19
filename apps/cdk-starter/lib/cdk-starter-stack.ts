b3038866d0980a27460c28defb61ae0b6a969c38
ACristobal <ajie.io@yahoo.com>
2026-04-18 21:29:06 -0400

    10 AWS CDK base project deployment

--- apps/cdk-starter/lib/cdk-starter-stack.ts
+++ apps/cdk-starter/lib/cdk-starter-stack.ts
@@ -1,15 +1,8 @@
 import * as cdk from "aws-cdk-lib";
-import * as sqs from "aws-cdk-lib/aws-sqs";
 import { Construct } from "constructs";
 
 export class CdkStarterStack extends cdk.Stack {
   constructor(scope: Construct, id: string, props?: cdk.StackProps) {
     super(scope, id, props);
-
-    // The code that defines your stack goes here
-    // example resource
-    const queue = new sqs.Queue(this, "CdkStarterQueue", {
-      visibilityTimeout: cdk.Duration.seconds(300),
-    });
   }
 }
