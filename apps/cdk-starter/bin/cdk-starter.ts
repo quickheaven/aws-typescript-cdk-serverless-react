f28b51d1da64208d2b3a18de656049306defd801
ACristobal <ajie.io@yahoo.com>
2026-05-01 21:32:40 -0400

    Fixed formatting

--- apps/cdk-starter/bin/cdk-starter.ts
+++ apps/cdk-starter/bin/cdk-starter.ts
@@ -3,7 +3,6 @@ import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { PhotosStack } from "../lib/PhotosStack";
 
-
 const app = new cdk.App();
 new PhotosStack(app, "PhotosStack", {
   env: {
