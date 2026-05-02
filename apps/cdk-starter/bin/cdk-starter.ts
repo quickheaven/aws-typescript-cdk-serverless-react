cbd13d135a4e794b9c3f5820ae20372ce554dd60
ACristobal <ajie.io@yahoo.com>
2026-05-01 21:32:09 -0400

    CDK IDs

--- apps/cdk-starter/bin/cdk-starter.ts
+++ apps/cdk-starter/bin/cdk-starter.ts
@@ -1,10 +1,11 @@
 #!/usr/bin/env node
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
-import { CdkStarterStack } from "../lib/cdk-starter-stack";
+import { PhotosStack } from "../lib/PhotosStack";
+
 
 const app = new cdk.App();
-new CdkStarterStack(app, "CdkStarterStack", {
+new PhotosStack(app, "PhotosStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
