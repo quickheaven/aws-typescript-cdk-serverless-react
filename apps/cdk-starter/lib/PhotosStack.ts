b0ad9619c02eb7d4cc4151fca9dd973b458fb6f8
ACristobal <ajie.io@yahoo.com>
2026-06-01 21:55:10 -0400

    21 Cross stack references

--- apps/cdk-starter/lib/PhotosStack.ts
+++ apps/cdk-starter/lib/PhotosStack.ts
@@ -1,7 +1,7 @@
 import { Construct } from "constructs";
 import * as cdk from "aws-cdk-lib";
 import { Bucket } from "aws-cdk-lib/aws-s3";
-import { Fn } from "aws-cdk-lib";
+import { CfnOutput, Fn } from "aws-cdk-lib";
 
 export class PhotosStack extends cdk.Stack {
   private stackSuffix: string;
@@ -11,8 +11,13 @@ export class PhotosStack extends cdk.Stack {
 
     this.initializeSuffix();
 
-    new Bucket(this, "PhotosBucket2", {
+    const photosBucket = new Bucket(this, "PhotosBucket2", {
       bucketName: `photos-bucket-${this.stackSuffix}`,
+    });
+
+    new CfnOutput(this, "photos-bucket", {
+      value: photosBucket.bucketArn,
+      exportName: "photos-bucket",
     });
   }
 
