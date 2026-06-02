41c51926dfaedf7a4b318e945e431b52a567dc36
ACristobal <ajie.io@yahoo.com>
2026-06-01 21:55:10 -0400

    21 Cross stack references

--- apps/cdk-starter/bin/cdk-starter.ts
+++ apps/cdk-starter/bin/cdk-starter.ts
@@ -2,9 +2,17 @@
 import "source-map-support/register";
 import * as cdk from "aws-cdk-lib";
 import { PhotosStack } from "../lib/PhotosStack";
+import { PhotosHandlerStack } from "../lib/PhotosHandlerStack";
 
 const app = new cdk.App();
 new PhotosStack(app, "PhotosStack", {
+  env: {
+    account: process.env.CDK_DEFAULT_ACCOUNT,
+    region: process.env.CDK_DEFAULT_REGION,
+  },
+});
+
+new PhotosHandlerStack(app, "PhotosHandlerStack", {
   env: {
     account: process.env.CDK_DEFAULT_ACCOUNT,
     region: process.env.CDK_DEFAULT_REGION,
