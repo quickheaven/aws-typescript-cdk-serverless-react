0a9cb2fae6b2869b379ac2b90d146e794a38b408
ACristobal <ajie.io@yahoo.com>
2026-07-29 23:29:30 -0400

    Fixed build error and formatting

--- apps/space-finder/src/infra/stacks/DataStack.ts
+++ apps/space-finder/src/infra/stacks/DataStack.ts
@@ -1,10 +1,8 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
-import { Construct } from 'constructs';
-
+import { Stack, StackProps } from "aws-cdk-lib";
+import { Construct } from "constructs";
 
 export class DataStack extends Stack {
-
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props)
-    }
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+  }
 }
