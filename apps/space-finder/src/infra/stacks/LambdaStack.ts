5a092521c3720d02c59eb2b9957e575261e15117
ACristobal <ajie.io@yahoo.com>
2026-07-31 21:22:41 -0400

    Fixed formatting

--- apps/space-finder/src/infra/stacks/LambdaStack.ts
+++ apps/space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,20 +1,20 @@
-import { Stack, StackProps } from 'aws-cdk-lib'
-import { Code, Function as LambdaFunction, Runtime} from 'aws-cdk-lib/aws-lambda';
-import { Construct } from 'constructs';
-import { join } from 'path';
-
+import { Stack, StackProps } from "aws-cdk-lib";
+import {
+  Code,
+  Function as LambdaFunction,
+  Runtime,
+} from "aws-cdk-lib/aws-lambda";
+import { Construct } from "constructs";
+import { join } from "path";
 
 export class LambdaStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
 
-    constructor(scope: Construct, id: string, props?: StackProps) {
-        super(scope, id, props)
-
-
-        new LambdaFunction(this, 'HelloLambda', {
-            runtime: Runtime.NODEJS_18_X,
-            handler: 'hello.main',
-            code: Code.fromAsset(join(__dirname, '..','..', 'services'))
-        })
-
-    }
+    new LambdaFunction(this, "HelloLambda", {
+      runtime: Runtime.NODEJS_18_X,
+      handler: "hello.main",
+      code: Code.fromAsset(join(__dirname, "..", "..", "services")),
+    });
+  }
 }
