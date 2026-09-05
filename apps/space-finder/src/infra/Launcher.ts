bf0cc7a21274d48633c5353dab872c3f8298b9fb
ACristobal <ajie.io@yahoo.com>
2026-09-05 15:51:08 -0400

    29 AWS DynamoDB with CDK

--- apps/space-finder/src/infra/Launcher.ts
+++ apps/space-finder/src/infra/Launcher.ts
@@ -4,8 +4,10 @@ import { LambdaStack } from "./stacks/LambdaStack";
 import { ApiStack } from "./stacks/ApiStack";
 
 const app = new App();
-new DataStack(app, "DataStack");
-const lambdaStack = new LambdaStack(app, "LambdaStack");
+const dataStack = new DataStack(app, 'DataStack');
+const lambdaStack = new LambdaStack(app, 'LambdaStack', {
+  spacesTable: dataStack.spacesTable
+})
 new ApiStack(app, "ApiStack", {
   helloLambdaIntegration: lambdaStack.helloLambdaIntegration,
 });
