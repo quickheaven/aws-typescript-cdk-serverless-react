b39e1d6aa9a5cf656c6ff5f58375e6c1cc64a86d
ACristobal <ajie.io@yahoo.com>
2026-09-05 15:51:08 -0400

    29 AWS DynamoDB with CDK

--- apps/space-finder/src/services/hello.js
+++ apps/space-finder/src/services/hello.js
@@ -1,8 +1,8 @@
 
 
-exports.main = async function(event, context) {
+exports.main = async function (event, context) {
     return {
         statusCode: 200,
-        body: JSON.stringify('Hello from lambda!')
+        body: JSON.stringify(`Hello! I will read from ${process.env.TABLE_NAME}`)
     }
 }
