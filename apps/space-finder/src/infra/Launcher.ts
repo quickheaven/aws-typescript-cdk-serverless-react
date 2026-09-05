aa75f13ddc4f0d3fc9f2800636af8b8141e08af9
ACristobal <ajie.io@yahoo.com>
2026-09-05 16:58:01 -0400

    Fixed formatting.

--- apps/space-finder/src/infra/Launcher.ts
+++ apps/space-finder/src/infra/Launcher.ts
@@ -4,10 +4,10 @@ import { LambdaStack } from "./stacks/LambdaStack";
 import { ApiStack } from "./stacks/ApiStack";
 
 const app = new App();
-const dataStack = new DataStack(app, 'DataStack');
-const lambdaStack = new LambdaStack(app, 'LambdaStack', {
-  spacesTable: dataStack.spacesTable
-})
+const dataStack = new DataStack(app, "DataStack");
+const lambdaStack = new LambdaStack(app, "LambdaStack", {
+  spacesTable: dataStack.spacesTable,
+});
 new ApiStack(app, "ApiStack", {
   helloLambdaIntegration: lambdaStack.helloLambdaIntegration,
 });
--- apps/space-finder/src/infra/stacks/LambdaStack.ts
+++ apps/space-finder/src/infra/stacks/LambdaStack.ts
@@ -1,6 +1,6 @@
 import { Stack, StackProps } from "aws-cdk-lib";
 import { LambdaIntegration } from "aws-cdk-lib/aws-apigateway";
-import { ITable } from 'aws-cdk-lib/aws-dynamodb';
+import { ITable } from "aws-cdk-lib/aws-dynamodb";
 import {
   Code,
   Function as LambdaFunction,
@@ -10,7 +10,7 @@ import { Construct } from "constructs";
 import { join } from "path";
 
 interface LambdaStackProps extends StackProps {
-  spacesTable: ITable
+  spacesTable: ITable;
 }
 
 export class LambdaStack extends Stack {
@@ -22,10 +22,10 @@ export class LambdaStack extends Stack {
     const helloLambda = new LambdaFunction(this, "HelloLambda", {
       runtime: Runtime.NODEJS_18_X,
       handler: "hello.main",
-      code: Code.fromAsset(join(__dirname, '..', '..', 'services')),
+      code: Code.fromAsset(join(__dirname, "..", "..", "services")),
       environment: {
-        TABLE_NAME: props.spacesTable.tableName
-      }
+        TABLE_NAME: props.spacesTable.tableName,
+      },
     });
 
     this.helloLambdaIntegration = new LambdaIntegration(helloLambda);
