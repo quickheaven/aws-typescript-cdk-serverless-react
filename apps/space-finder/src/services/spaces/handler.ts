cfe81376984940fdf0591abc250d3507b52ef52e
ACristobal <ajie.io@yahoo.com>
2026-09-06 10:35:32 -0400

    37 Initial spaces, lambda

---
+++ apps/space-finder/src/services/spaces/handler.ts
@@ -0,0 +1,33 @@
+import {
+  APIGatewayProxyEvent,
+  APIGatewayProxyResult,
+  Context,
+} from "aws-lambda";
+
+async function handler(
+  event: APIGatewayProxyEvent,
+  context: Context,
+): Promise<APIGatewayProxyResult> {
+  let message: string;
+
+  switch (event.httpMethod) {
+    case "GET":
+      message = "Hello from GET!";
+      break;
+    case "POST":
+      message = "Hello from POST!";
+      break;
+    default:
+      message = "Method not allowed";
+      break;
+  }
+
+  const response: APIGatewayProxyResult = {
+    statusCode: 200,
+    body: JSON.stringify(message),
+  };
+
+  return response;
+}
+
+export { handler };
