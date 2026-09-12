f24b645129b3cac128163245819167f55930ff94
ACristobal <ajie.io@yahoo.com>
2026-09-12 17:48:32 -0400

    43 Marshalling

--- apps/space-finder/src/services/spaces/GetSpaces.ts
+++ apps/space-finder/src/services/spaces/GetSpaces.ts
@@ -3,6 +3,7 @@ import {
   GetItemCommand,
   ScanCommand,
 } from "@aws-sdk/client-dynamodb";
+import { unmarshall } from "@aws-sdk/util-dynamodb";
 import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
 
 export async function getSpaces(
@@ -21,9 +22,10 @@ export async function getSpaces(
         }),
       );
       if (getItemResponse.Item) {
+        const unmashalledItem = unmarshall(getItemResponse.Item);
         return {
           statusCode: 200,
-          body: JSON.stringify(getItemResponse.Item),
+          body: JSON.stringify(unmashalledItem),
         };
       } else {
         return {
@@ -44,10 +46,11 @@ export async function getSpaces(
       TableName: process.env.TABLE_NAME,
     }),
   );
-  console.log(result.Items);
+  const unmashalledItems = result.Items?.map((item) => unmarshall(item));
+  console.log(unmashalledItems);
 
   return {
     statusCode: 201,
-    body: JSON.stringify(result.Items),
+    body: JSON.stringify(unmashalledItems),
   };
 }
