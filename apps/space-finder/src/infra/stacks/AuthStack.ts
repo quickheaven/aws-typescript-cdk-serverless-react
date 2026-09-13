0ba0af000a71314d2b63256f07af0c417ceb3b03
ACristobal <ajie.io@yahoo.com>
2026-09-13 17:28:26 -0400

    66 Testing AWS temporary credentials

--- apps/space-finder/src/infra/stacks/AuthStack.ts
+++ apps/space-finder/src/infra/stacks/AuthStack.ts
@@ -6,7 +6,12 @@ import {
   UserPool,
   UserPoolClient,
 } from "aws-cdk-lib/aws-cognito";
-import { FederatedPrincipal, Role } from "aws-cdk-lib/aws-iam";
+import {
+  Effect,
+  FederatedPrincipal,
+  PolicyStatement,
+  Role,
+} from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
 export class AuthStack extends Stack {
@@ -22,10 +27,10 @@ export class AuthStack extends Stack {
 
     this.createUserPool();
     this.createUserPoolClient();
-    this.createAdminsGroup();
     this.createIdentityPool();
     this.createRoles();
     this.attachRoles();
+    this.createAdminsGroup();
   }
 
   private createUserPool() {
@@ -59,6 +64,7 @@ export class AuthStack extends Stack {
     new CfnUserPoolGroup(this, "SpaceAdmins", {
       userPoolId: this.userPool.userPoolId,
       groupName: "admins",
+      roleArn: this.adminRole.roleArn,
     });
   }
 
@@ -124,6 +130,14 @@ export class AuthStack extends Stack {
         "sts:AssumeRoleWithWebIdentity",
       ),
     });
+
+    this.adminRole.addToPolicy(
+      new PolicyStatement({
+        effect: Effect.ALLOW,
+        actions: ["s3:ListAllMyBuckets"],
+        resources: ["*"],
+      }),
+    );
   }
 
   private attachRoles() {
--- apps/space-finder/test/auth.test.ts
+++ apps/space-finder/test/auth.test.ts
@@ -1,3 +1,4 @@
+import { ListBucketsCommand, S3Client } from "@aws-sdk/client-s3";
 import { AuthService } from "./AuthService";
 
 test("auth login and get id token", async () => {
@@ -9,5 +10,19 @@ test("auth login and get id token", async () => {
   const idToken = await service.getIdToken();
   console.log(idToken);
   expect(idToken).toBeDefined();
+
   const credentials = await service.generateTemporaryCredentials();
+  console.log(credentials);
+
+  const buckets = await listBuckets(credentials);
+  console.log(buckets);
 });
+
+async function listBuckets(credentials: any) {
+  const client = new S3Client({
+    credentials: credentials,
+  });
+  const command = new ListBucketsCommand({});
+  const result = await client.send(command);
+  return result;
+}
