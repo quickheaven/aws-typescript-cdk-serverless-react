acc7103f508ee7f526901373359b6b1f019c0e92
ACristobal <ajie.io@yahoo.com>
2026-09-13 15:34:55 -0400

    fix: remove redundant dotenv/config from jest setupFiles

--- apps/space-finder/jest.config.js
+++ apps/space-finder/jest.config.js
@@ -1,7 +1,6 @@
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
     testEnvironment: 'node',
-    setupFiles: ['dotenv/config'],
     testMatch: ['**/test/**/*.test.ts'],
     transform: {
         '^.+\\.tsx?$': ['ts-jest', { tsconfig: './tsconfig.json' }]
