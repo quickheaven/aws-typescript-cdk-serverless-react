67c8ee463e1010e2271d94f3898ed92798469927
ACristobal <ajie.io@yahoo.com>
2026-09-13 16:26:19 -0400

    63 Identity pools in CDK

--- apps/space-finder/src/infra/stacks/AuthStack.ts
+++ apps/space-finder/src/infra/stacks/AuthStack.ts
@@ -1,5 +1,6 @@
 import { CfnOutput, Stack, StackProps } from "aws-cdk-lib";
 import {
+  CfnIdentityPool,
   CfnUserPoolGroup,
   UserPool,
   UserPoolClient,
@@ -9,6 +10,7 @@ import { Construct } from "constructs";
 export class AuthStack extends Stack {
   public userPool: UserPool;
   private userPoolClient: UserPoolClient;
+  private identityPool: CfnIdentityPool;
 
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
@@ -16,6 +18,7 @@ export class AuthStack extends Stack {
     this.createUserPool();
     this.createUserPoolClient();
     this.createAdminsGroup();
+    this.createIdentityPool();
   }
 
   private createUserPool() {
@@ -49,6 +52,21 @@ export class AuthStack extends Stack {
     new CfnUserPoolGroup(this, "SpaceAdmins", {
       userPoolId: this.userPool.userPoolId,
       groupName: "admins",
+    });
+  }
+
+  private createIdentityPool() {
+    this.identityPool = new CfnIdentityPool(this, "SpaceIdentityPool", {
+      allowUnauthenticatedIdentities: true,
+      cognitoIdentityProviders: [
+        {
+          clientId: this.userPoolClient.userPoolClientId,
+          providerName: this.userPool.userPoolProviderName,
+        },
+      ],
+    });
+    new CfnOutput(this, "SpaceIdentityPoolId", {
+      value: this.identityPool.ref,
     });
   }
 }
